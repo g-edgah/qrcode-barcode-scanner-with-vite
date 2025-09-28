36d8d395e0593c96c78919ad2a3a02ae72f41d55
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-09-28 21:03:24 +0300

    buttons to start and stop scanning

--- src/backcamera.js
+++ src/backcamera.js
@@ -1,4 +1,9 @@
-const html5Qrcode = new Html5Qrcode('reader', true);
+const html5Qrcode = new Html5Qrcode('reader', /*true*/);
+/*document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#stopScan').style.display = 'none';
+    document.querySelector('#reader').style.display = 'none';
+}); */
+
 const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     // succesfull scan handling
     document.querySelector('#result').innerHTML= `Decoded Text = ${decodedText}`;
@@ -14,5 +19,21 @@ const config = {
 
 };
 
+
+document.querySelector('#startScan').addEventListener('click', () => {
+    //prefer back camera
+    document.querySelector('#reader').style.display = 'block';
+    document.querySelector('#stopScan').style.display = 'block';
+    document.querySelector('#startScan').style.display = 'none';
+
+    html5Qrcode.start({facingMode: 'environment'}, config, qrCodeSuccessCallback);
+});
+
+
+document.querySelector('#stopScan').addEventListener('click', () => {
+    document.querySelector('#reader').style.display = 'none';
+    document.querySelector('#startScan').style.display = 'block';
+    document.querySelector('#stopScan').style.display = 'none';
 //prefer back camera
-html5Qrcode.start({facingMode: 'environment'}, config, qrCodeSuccessCallback);
+html5Qrcode.stop({facingMode: 'environment'}, config, qrCodeSuccessCallback);
+});
